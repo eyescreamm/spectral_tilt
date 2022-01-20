68561499a10ba0fab2a7e31cffe121fd706234fd
ShutoYoshida <52554194+ShutoYoshida@users.noreply.github.com>
2022-01-20 15:55:25 +0900

    Add files via upload

--- tilt~.c
+++ tilt~.c
@@ -1,5 +1,6 @@
 #include "m_pd.h"
 #include <math.h>
+#include <stdlib.h>
 
 /* class declaration (data space) */
 static t_class *tilt_tilde_class;
@@ -9,6 +10,10 @@ typedef struct t_tilt_tilde {
 
   t_float tilt;
   t_float num;
+  t_float sr;
+  t_float f0;
+  t_float w0;
+  t_float r;
   t_inlet *x_in2;
   t_inlet *x_in3;
 
@@ -16,47 +21,32 @@ typedef struct t_tilt_tilde {
 } t_tilt_tilde;
 
 t_int *tilt_tilde_perform(t_int *w) {
-  t_tilt_tilde  *x         =     (t_tilt_tilde *)(w[1]);
+  t_tilt_tilde    *x         =     (t_tilt_tilde *)(w[1]);
   t_sample        *in        =     (t_sample *)(w[2]);
   t_sample        *out       =     (t_sample *)(w[3]);
   int              n         =     (int)(w[4]);
 
-  // double samprate = sys_getsr();
-  double f0 = 20;
-  double w0 = 2 * 3.141593 * f0;
-  double tilt = x->tilt;
-  int num = x->num;
-
-  // post("pi = %lf\n", M_PI);
-  // post("w0 = %lf\n", w0);
-
   /* decide poles and zeros */
-
-  double mp[num],mz[num];
-  double r = 1.2;
-
-  for(int i=0; i<num; i++) {
-    mp[i] = (t_sample)(w0 * pow(r,i));
-    mz[i] = (t_sample)(w0 * pow(r,i - tilt));
+  int num = x->num;
+  t_float mp[num],mz[num];
+  for(int i=0; i<=num-1; i++) {
+    mp[i] = (t_sample)(x->w0 * pow(x->r,i));
+    mz[i] = (t_sample)(x->w0 * pow(x->r,i - x->tilt));
   }
 
   /* prewarp method */
-
-  double mph[num],mzh[num];
-  // double sr = 2 * n;
-  double sr = 2 * 44100;
-
-  for(int i=0; i<num; i++) {
-      mph[i] = (t_sample)(w0*(tan(mp[i]/sr))/(tan((w0)/sr)));
-      mzh[i] = (t_sample)(w0*(tan(mz[i]/sr))/(tan((w0)/sr)));
+  t_float mph[num],mzh[num];
+  t_float tsr = 2 * x->sr;
+  for(int i=0; i<=num-1; i++) {
+      mph[i] = (t_sample)(x->w0*(tan(mp[i]/tsr))/(tan((x->w0)/tsr)));
+      mzh[i] = (t_sample)(x->w0*(tan(mz[i]/tsr))/(tan((x->w0)/tsr)));
   }
 
   /* bilinear transform */
-
-  double g[num],b1[num],b0[num],a1[num];
-  double c  = 1.0/tan(1*0.5/44100); //bilinear-transform scale-factor, w1=1
-  double d;
-  for(int i=0; i<num; i++){
+  t_float g[num],b1[num],b0[num],a1[num];
+  t_float c  = 1.0/tan(1*0.5/x->sr); //bilinear-transform scale-factor, w1=1
+  t_float d;
+  for(int i=0; i<=num-1; i++){
     d     =  mph[i] + c;
     g[i]  =  mph[i] / mzh[i];
     b1[i] =  (mzh[i]-c)/d;
@@ -65,44 +55,40 @@ t_int *tilt_tilde_perform(t_int *w) {
   }
 
   /* filter function */
-
-  double sigy[64];
-  for(int i=0; i<num; i++){
-    for(int j=1; j<64; j++){
+  t_float sigy[n];
+  for(int i=0; i<=num-1; i++){
+    for(int j=1; j<n; j++){
       sigy[0] = b0[i] * in[0];
       sigy[j] = b0[i]*in[j] + b1[i]*in[j-1] - a1[i]*sigy[j-1];
     }
-    for(int k=0; k<64; k++){
+    for(int k=0; k<n; k++){
       in[k] = g[i] * sigy[k];
     }
   }
 
-  // double iin[64]={-0.9996, 0.7176, -0.5931, -0.5716, 0.3742, -0.4732, 0.08887, 0.9708, 0.8544, 0.2746,  0.5327, -0.9613, -0.6775, 0.8326, -0.00247, -0.6964, -0.2683, 0.3873, -0.3654, -0.5056, 0.1229, 0.5495, 0.2529, -0.2438, -0.8605, 0.9637, -0.4152, -0.4898, 0.04398, -0.5825, -0.9485, 0.8127, 0.233, 0.4509, 0.02883, 0.2148, 0.253, 0.4658, -0.387, -0.8784, 0.316, -0.6648, -0.1142, 0.6513, 0.3154, 0.5127, 0.6808, 0.9951, -0.659, -0.3581, -0.1517, -0.7237, 0.6952, -0.3345, -0.9784, -0.09022, 0.4085, 0.4351, 0.8822, -0.5389, -0.7452, 0.8205, -0.07444, 0.006087};
-  //
-  // for(int i=0; i<num; i++){
-  //   for(int j=1; j<n; j++){
-  //     sigy[0] = b0[i] * iin[0];
-  //     sigy[j] = b0[i]*iin[j] + b1[i]*iin[j-1] - a1[i]*sigy[j-1];
-  //   }
-  //   for(int k=0; k<n; k++){
-  //     iin[k] = g[i] * sigy[k];
-  //   }
-  // }
-
   /* mean function */
-
-  double x_ave = 0;
-  for(int i=0; i<64; i++){
+  t_float x_ave = 0;
+  for(int i=0; i<n; i++){
     x_ave += in[i];
   }
-  x_ave = x_ave/(double)n;
-
-  for(int i=0; i<64; i++){
+  x_ave = x_ave/n;
+  // t_float max=0;
+  // t_float tmp[n];
+  for(int i=0; i<n; i++){
+    // tmp[i] = in[i] - x_ave;
+    // if(fabsf(tmp[i])>max){
+    //   max = fabsf(tmp[i]);
+    // }
     out[i] = in[i] - x_ave;
   }
-
-  // for(int i=0; i<n; i++){
-  //   out[i] = iin[i];
+  // for(int i=0; i<40; i++){
+  //   if(max<1){
+  //     for(int j=0; j<n; j++){
+  //       out[j] = tmp[j]/pow(10,i);
+  //     }
+  //     break;
+  //   }
+  //   max=max/10;
   // }
 
   return (w + 5);
@@ -110,6 +96,10 @@ t_int *tilt_tilde_perform(t_int *w) {
 
 /* register singen_tilde to the dsp tree */
 void tilt_tilde_dsp(t_tilt_tilde *x, t_signal **sp) {
+  x->sr = sys_getsr();
+  x->f0 = 20;
+  x->w0 = 2 * 3.14159265358979323846 * x->f0;
+  x->r = 1.2;
   dsp_add(tilt_tilde_perform,
       4, /* number of following pointers */
       x,
@@ -129,6 +119,13 @@ void tilt_tilde_free(t_tilt_tilde *x) {
 void *tilt_tilde_new(t_floatarg f) {
   t_tilt_tilde *x = (t_tilt_tilde *)pd_new(tilt_tilde_class);
   x->num = f;
+  if(f<0){
+    post("error: Arguments with values less than 0 are not allowed.");
+    return (void *)x;
+  } else if(f>150){
+    post("error: The value of the argument is too large. The system will not work properly.");
+    return (void *)x;
+  }
   x->x_in2 = floatinlet_new(&x->x_obj, &x->tilt);
   x->x_in3 = floatinlet_new(&x->x_obj, &x->num);
   x->x_out = outlet_new(&x->x_obj, &s_signal);
